95a23ed9cd2597405cb641350af187b46e91e4e8
Dmytro Korotkiy <dmitriy1597531@gmail.com>
2024-09-27 18:26:13 +0300

    feat: added dynamic product cards and products cards styles

--- src/js/main.js
+++ src/js/main.js
@@ -1,66 +1,94 @@
 import '../styles/main.scss'
+import halfPriceIcon from '../public/svg/icon_half_price.svg'
+import buttonIcon from '../public/svg/icon_button_download.svg'
 
-// document.addEventListener('DOMContentLoaded', function () {
-//   const itemsContainer = document.querySelector('.product-section__items')
-//   console.log(itemsContainer)
-//   const downloadArrow = document.querySelector('.download-arrow')
+document.addEventListener('DOMContentLoaded', function () {
+  const itemsContainer = document.querySelector('.product-section__items')
+  console.log(itemsContainer)
+  const downloadArrow = document.querySelector('.download-arrow')
+  console.log(halfPriceIcon)
 
-//   // Fetch JSON data using AJAX
-//   fetch('https://veryfast.io/t/front_test_api.php')
-//     .then((response) => response.json())
-//     .then((data) => data.result.elements)
-//     .then((data) => {
-//       data.forEach((item) => {
-//         const itemElement = document.createElement('div')
-//         itemElement.className = 'product-card'
-//         itemElement.innerHTML = `
-//                     <h2>${item.name_display}</h2>
-//                     <p>License: ${item.license_name}</p>
-//                     <p>Price: ${item.price_key}</p>
-//                     <p>Best Seller: ${item.is_best ? 'Yes' : 'No'}</p>
-//                     <button class="download-btn" data-link="${
-//                       item.link
-//                     }">Download</button>
-//                 `
-//         itemsContainer.appendChild(itemElement)
-//       })
+  // Fetch JSON data using AJAX
+  fetch('https://veryfast.io/t/front_test_api.php')
+    .then((response) => response.json())
+    .then((data) => data.result.elements)
+    .then((data) => {
+      data.forEach((item) => {
+        const itemElement = document.createElement('article')
+        itemElement.className = 'product-card'
+        itemElement.innerHTML = `
+              <div class="product-card__price">
+                ${
+                  item.is_best
+                    ? `<div class="product-card__price-badge">Best value</div>`
+                    : ''
+                }
+                ${
+                  item.price_key === '50%'
+                    ? `<div class="product-card__price-half">
+                        <img src='${halfPriceIcon}' alt='Half price icon' />          
+                      </div>`
+                    : ''
+                }
+                <span class="product-card__price-text">$${item.amount}</span>
+                <span class="product-card__price-period">${
+                  item.price_key === '50%' ? '/mo' : '/per year'
+                }</span>
+              </div>
+              <div class="product-card__description">
+                <span class="product-card__description-title"
+                  >${item.name_display}</span
+                >
+                <span class="product-card__description-text"
+                  >${item.license_name}</span
+                >
+              </div>
+              <button data-link='${
+                item.link
+              }' class="button download-button product-card__download-button">
+                <span class="button__text">Download</span>
+                <img src='${buttonIcon}' alt='Button download icon' />
+              </button>
+                `
+        itemsContainer.appendChild(itemElement)
+      })
 
-//       // Add click event listener to download buttons
-//       const downloadButtons = document.querySelectorAll('.download-btn')
-//       downloadButtons.forEach((button) => {
-//         button.addEventListener('click', function () {
-//           const link = this.getAttribute('data-link')
-//           downloadFile(link)
-//         })
-//       })
-//     })
-//     .catch((error) => console.error('Error fetching data:', error))
+      // Add click event listener to download buttons
+      const downloadButtons = document.querySelectorAll('.download-button')
+      downloadButtons.forEach((button) => {
+        button.addEventListener('click', function () {
+          const link = this.getAttribute('data-link')
+          downloadFile(link)
+        })
+      })
+    })
+    .catch((error) => console.error('Error fetching data:', error))
 
-//   function downloadFile(url) {
-//     const a = document.createElement('a')
-//     a.href = url
-//     a.download = url.substring(url.lastIndexOf('/') + 1)
-//     document.body.appendChild(a)
-//     a.click()
-//     document.body.removeChild(a)
+  function downloadFile(url) {
+    const a = document.createElement('a')
+    a.href = url
+    a.download = url.substring(url.lastIndexOf('/') + 1)
+    document.body.appendChild(a)
+    a.click()
+    document.body.removeChild(a)
 
-//     showDownloadArrow()
-//   }
+    showDownloadArrow()
+  }
 
-//   function showDownloadArrow() {
-//     if (downloadArrow.classList.contains('hidden')) {
-//       downloadArrow.classList.remove('hidden')
-//     }
+  function showDownloadArrow() {
+    if (downloadArrow.classList.contains('hidden')) {
+      downloadArrow.classList.remove('hidden')
+    }
 
-//     // Adjust arrow position based on browser
-//     if (navigator.userAgent.indexOf('Chrome') !== -1) {
-//       downloadArrow.className = 'arrow down'
-//       downloadArrow.style.left = '50%'
-//       downloadArrow.style.transform = 'translateX(-50%)'
-//     } else if (navigator.userAgent.indexOf('Firefox') !== -1) {
-//       downloadArrow.className = 'arrow right'
-//       downloadArrow.style.top = '50%'
-//       downloadArrow.style.transform = 'translateY(-50%)'
-//     }
-//   }
-// })
+    // Adjust arrow position based on browser
+    if (navigator.userAgent.indexOf('Chrome') !== -1) {
+      downloadArrow.className = 'arrow down'
+      downloadArrow.style.left = '50%'
+      downloadArrow.style.transform = 'translateX(-50%)'
+    } else if (navigator.userAgent.indexOf('Firefox') !== -1) {
+      downloadArrow.className = 'arrow right'
+      downloadArrow.style.top = '50%'
+      downloadArrow.style.transform = 'translateY(-50%)'
+    }
+  }
+})
